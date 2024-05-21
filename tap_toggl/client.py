ccf58bb233ca7d604976e5b49498f44b202404c2
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-05-21 10:03:01 -0600

    Fix incremental replication

--- tap_toggl/client.py
+++ tap_toggl/client.py
@@ -12,7 +12,7 @@ class TogglStream(RESTStream[t.Any]):
     """Toggl stream class."""
 
     url_base = "https://api.track.toggl.com/api/v9"
-    records_jsonpath = "$[*]"  # Or override `parse_response`.
+    records_jsonpath = "$[*]"
     next_page_token_jsonpath = "$.next_page"  # noqa: S105
 
     @property
@@ -42,7 +42,7 @@ class TogglStream(RESTStream[t.Any]):
 
     def get_url_params(
         self,
-        context: dict[str, t.Any] | None,
+        context: dict[str, t.Any] | None,  # noqa: ARG002
         next_page_token: t.Any | None,  # noqa: ARG002, ANN401
     ) -> dict[str, t.Any]:
         """Get URL query parameters.
@@ -55,7 +55,30 @@ class TogglStream(RESTStream[t.Any]):
             Mapping of URL query parameters.
         """
         params: dict[str, t.Any] = {}
-        if start_date := self.get_starting_timestamp(context):
-            params["since"] = int(start_date.timestamp())
+        return params
 
+
+class IncrementalTogglStream(TogglStream):
+    """Toggl stream class with incremental sync."""
+
+    state_pk = "at"
+    replication_key = "at"
+
+    def get_url_params(
+        self,
+        context: dict[str, t.Any] | None,
+        next_page_token: t.Any | None,  # noqa: ANN401
+    ) -> dict[str, t.Any]:
+        """Get URL query parameters.
+
+        Args:
+            context: Stream sync context.
+            next_page_token: Next offset.
+
+        Returns:
+            Mapping of URL query parameters.
+        """
+        params = super().get_url_params(context, next_page_token)
+        if context and context.get(self.replication_key):
+            params["start_date"] = context.get(self.replication_key)
         return params
