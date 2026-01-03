9b8da6fffbc601ecaad336c2fff24a22e9d6f65a
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2026-01-02 22:22:31 -0600

    fix: Fixed incremental replication for the `time_entries` stream (#249)
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>

--- tap_toggl/client.py
+++ tap_toggl/client.py
@@ -2,29 +2,23 @@
 
 from __future__ import annotations
 
-import typing as t
+from typing import Any, override
 
 from requests.auth import HTTPBasicAuth
 from singer_sdk import RESTStream
 
-if t.TYPE_CHECKING:
-    from singer_sdk.helpers.types import Context
 
-
-class TogglStream(RESTStream[t.Any]):
+class TogglStream(RESTStream[Any]):
     """Toggl stream class."""
 
     url_base = "https://api.track.toggl.com/api/v9"
     records_jsonpath = "$[*]"
     next_page_token_jsonpath = "$.next_page"  # noqa: S105
 
+    @override
     @property
     def authenticator(self) -> HTTPBasicAuth:
-        """Get an authenticator object.
-
-        Returns:
-            The authenticator instance for this REST stream.
-        """
+        """Get an authenticator for the Toggl API."""
         if api_token := self.config.get("api_token"):
             return HTTPBasicAuth(api_token, "api_token")
         if username := self.config.get("username"):
@@ -33,55 +27,3 @@ class TogglStream(RESTStream[t.Any]):
 
         msg = "Invalid auth configuration"
         raise ValueError(msg)
-
-    @property
-    def http_headers(self) -> dict[str, str]:
-        """Return the http headers needed.
-
-        Returns:
-            A dictionary of HTTP headers.
-        """
-        return {"User-Agent": f"{self.tap_name}/{self._tap.plugin_version}"}
-
-    def get_url_params(
-        self,
-        context: Context | None,  # noqa: ARG002
-        next_page_token: t.Any | None,  # noqa: ARG002, ANN401
-    ) -> dict[str, t.Any]:
-        """Get URL query parameters.
-
-        Args:
-            context: Stream sync context.
-            next_page_token: Next offset.
-
-        Returns:
-            Mapping of URL query parameters.
-        """
-        params: dict[str, t.Any] = {}
-        return params
-
-
-class IncrementalTogglStream(TogglStream):
-    """Toggl stream class with incremental sync."""
-
-    state_pk = "at"
-    replication_key = "at"
-
-    def get_url_params(
-        self,
-        context: Context | None,
-        next_page_token: t.Any | None,  # noqa: ANN401
-    ) -> dict[str, t.Any]:
-        """Get URL query parameters.
-
-        Args:
-            context: Stream sync context.
-            next_page_token: Next offset.
-
-        Returns:
-            Mapping of URL query parameters.
-        """
-        params = super().get_url_params(context, next_page_token)
-        if context and context.get(self.replication_key):
-            params["start_date"] = context.get(self.replication_key)
-        return params
